8323a92b101a6450f3048e3850451846d346a30b
vertigo <shadman71@gmail.com>
2014-07-01 13:20:28 +0100

    Revert "Updated loki and various other fixes"
    This reverts commit 4158b21a1d6e84609aec0446f527437588028b15.

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -29,7 +29,6 @@
 
 #include <utils/threads.h>
 #include <utils/String8.h>
-#include <utils/Errors.h>
 #include <hardware/hardware.h>
 #include <hardware/camera.h>
 #include <camera/Camera.h>
@@ -54,7 +53,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
          version_major: 1,
          version_minor: 0,
          id: CAMERA_HARDWARE_MODULE_ID,
-         name: "G2 Camera Wrapper",
+         name: "Flex Camera Wrapper",
          author: "The CyanogenMod Project",
          methods: &camera_module_methods,
          dso: NULL, /* remove compilation warnings */
@@ -254,7 +253,7 @@ int camera_start_preview(struct camera_device * device)
         return -EINVAL;
 
     rc = VENDOR_CALL(device, start_preview);
-    previewRunning = (rc == android::NO_ERROR);
+    previewRunning = rc > 0;
     return rc;
 }
 
